313aff82248c4739085bbf77616ba1d22324fc64
Samyuktha-Dasari24 <73239211+Samyuktha-Dasari24@users.noreply.github.com>
2020-10-22 09:27:15 +0530

    Update PersonRepository.java

--- BasicCRUD/src/main/java/com/selflearn/repository/PersonRepository.java
+++ BasicCRUD/src/main/java/com/selflearn/repository/PersonRepository.java
@@ -2,6 +2,7 @@ package com.selflearn.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.selflearn.model.Person;
+//repository interface
 public interface PersonRepository extends CrudRepository<Person, Integer>
 {
 }
