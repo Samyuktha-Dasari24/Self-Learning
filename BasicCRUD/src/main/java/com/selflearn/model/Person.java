5b6cef6e9e83cb102389a3b6042810586cc7eaaa
Samyuktha-Dasari24 <73239211+Samyuktha-Dasari24@users.noreply.github.com>
2020-10-22 09:21:32 +0530

    Create Person.java

---
+++ BasicCRUD/src/main/java/com/selflearn/model/Person.java
@@ -0,0 +1,57 @@
+package com.selflearn.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//mark class as an Entity 
+@Entity
+//defining class name as Table name
+@Table
+public class Person
+{
+//Defining id as primary key
+@Id
+@Column
+private int id;
+@Column
+private String firstName;
+@Column
+private String lastName;
+/**
+ * @return the id
+ */
+public int getId() {
+	return id;
+}
+/**
+ * @param id the id to set
+ */
+public void setId(int id) {
+	this.id = id;
+}
+/**
+ * @return the firstName
+ */
+public String getFirstName() {
+	return firstName;
+}
+/**
+ * @param firstName the firstName to set
+ */
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+/**
+ * @return the lastName
+ */
+public String getLastName() {
+	return lastName;
+}
+/**
+ * @param lastName the lastName to set
+ */
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+}
