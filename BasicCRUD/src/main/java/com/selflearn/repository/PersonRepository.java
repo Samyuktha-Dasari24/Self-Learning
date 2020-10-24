def650a4f118ca5ab5a42faf7777275d9c4de46f
Samyuktha-Dasari24 <73239211+Samyuktha-Dasari24@users.noreply.github.com>
2020-10-24 23:52:14 +0530

    Update PersonRepository.java

--- BasicCRUD/src/main/java/com/selflearn/repository/PersonRepository.java
+++ BasicCRUD/src/main/java/com/selflearn/repository/PersonRepository.java
@@ -1,8 +1,15 @@
 package com.selflearn.repository;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.selflearn.model.Person;
-//repository interface
-public interface PersonRepository extends CrudRepository<Person, Integer>
-{
+
+/**
+ * @author SONY
+ *
+ */
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+	public Person findPersonById(Long personId);
 }
