a8faae36f78fae02aae1fadfc085752d6ce55d1c
Samyuktha-Dasari24 <73239211+Samyuktha-Dasari24@users.noreply.github.com>
2020-10-26 23:23:04 +0530

    Update SpringBootCrudOperationApplicationTests.java

--- BasicCRUD/src/test/java/com/selflearn/SpringBootCrudOperationApplicationTests.java
+++ BasicCRUD/src/test/java/com/selflearn/SpringBootCrudOperationApplicationTests.java
@@ -9,10 +9,8 @@ import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.selflearn.controller.AddressController;
 import com.selflearn.controller.PersonController;
@@ -23,7 +21,6 @@ import com.selflearn.model.Person;
  *
  */
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootCrudOperationApplicationTests {
 
