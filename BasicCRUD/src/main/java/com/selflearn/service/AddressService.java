2dacfbc9edf7f22e7821b36c34495b2c8369a97f
Samyuktha-Dasari24 <73239211+Samyuktha-Dasari24@users.noreply.github.com>
2020-10-27 16:03:55 +0530

    Update AddressService.java

--- BasicCRUD/src/main/java/com/selflearn/service/AddressService.java
+++ BasicCRUD/src/main/java/com/selflearn/service/AddressService.java
@@ -5,6 +5,10 @@ package com.selflearn.service;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,30 +24,37 @@ public class AddressService {
 	@Autowired
 	AddressRepository addressRepository;
 
+	private static final Logger logger = LoggerFactory.getLogger(AddressService.class);
+	
 	// getting all persons record by using the method findaAll() of JPARepository
 	public List<Address> getAllAddress() {
-		//List<Address> address = new ArrayList<Address>();
+		logger.info("Address -> getAllAddress()");
 		return addressRepository.findAll();
-				//.forEach(address1 -> address.add(address1));
 		}
 
 	// saving a specific record by using the method save() of JPARepository
 	public Address saveOrUpdate(Address address) {
+		logger.info("Address -> saveOrUpdate()");
 		Address updatedAddress = addressRepository.save(address);
+		logger.info("Address -> updatedAddress : " +updatedAddress.getAddress_id());
 		return updatedAddress;
 	}
 
 	// deleting a specific record by using the method deleteById() of JPARepository
 	public void delete(Long id) {
+		logger.info("Address -> deleteAddress()");
+		try {
 		Address address = addressRepository.getOne(id);
-		if(address != null) {
 			if(address.getPerson() != null) {
 				address.setPerson(null);
-				System.out.println("Address with id is found and about to delete");
+				logger.info("Address with id is found and about to delete");
 				addressRepository.delete(address);
+			}else {
+				logger.info("Address with id is not found and not able to delete");
 			}
-		}else {
-			System.out.println("No address with id is found");
+		}catch(EntityNotFoundException ex) {
+			ex.printStackTrace();
+	           logger.error(ex.getMessage(), ex);
 		}
 			}
 }
